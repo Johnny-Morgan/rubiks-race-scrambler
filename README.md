004ca879401400d23a247b6e19e7fde75070f009
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2021-01-08 21:21:01 +0000

    Add code to manipulate grid borders

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## This website is an online version of the scrambler for the rubiks race game.
 
-#### - The scrambler contains 9 die which make up a 9 x 9 grid. Each dice has 6 sides with 6 colours namely red, orange, green, blue, yellow and white.
+#### - The scrambler contains 9 die which make up a 3 x 3 grid. Each dice has 6 sides with 6 colours namely red, orange, green, blue, yellow and white.
 
-#### - The player shakes the container containing the 9 die at the start of each game. This website is a replacement for the physical die, the player simply presses the shuffle button which generates a 9 x 9 grid with each square in the grid representing a dice.
+#### - The player shakes the container containing the 9 die at the start of each game. This website is a replacement for the physical die, the player simply presses the shuffle button which generates a 3 x 3 grid with each square in the grid representing a dice.
+
+#### - The player has the option to increase the size of the grid to a 4 x 4 grid or a 5 x 5 grid.
--- script.js
+++ script.js
@@ -33,6 +33,13 @@ document
       document.getElementById(`square-${i}-3`).classList.add("hidden");
       document.getElementById(`square-${i}-4`).classList.add("hidden");
     }
+    // Add right and bottom borders
+    document.getElementById(`square-0-2`).classList.add("border-right");
+    document.getElementById(`square-1-2`).classList.add("border-right");
+    document.getElementById(`square-2-2`).classList.add("border-right");
+    document.getElementById(`square-2-0`).classList.add("border-btm");
+    document.getElementById(`square-2-1`).classList.add("border-btm");
+    document.getElementById(`square-2-2`).classList.add("border-btm");
   });
 
 document.getElementById("four-by-four").addEventListener("click", function () {
@@ -42,6 +49,23 @@ document.getElementById("four-by-four").addEventListener("click", function () {
     document.getElementById(`square-${i}-3`).classList.remove("hidden");
     document.getElementById(`square-${i}-4`).classList.add("hidden");
   }
+  // Remove right border when clicking to 4 X 4 from 3 X 3
+  document.getElementById(`square-0-2`).classList.remove("border-right");
+  document.getElementById(`square-1-2`).classList.remove("border-right");
+  document.getElementById(`square-2-2`).classList.remove("border-right");
+  // Remove bottom border when clicking to 4 X 4 from 3 X 3
+  document.getElementById(`square-2-0`).classList.remove("border-btm");
+  document.getElementById(`square-2-1`).classList.remove("border-btm");
+  document.getElementById(`square-2-2`).classList.remove("border-btm");
+  // Add right and bottom borders
+  document.getElementById(`square-0-3`).classList.add("border-right");
+  document.getElementById(`square-1-3`).classList.add("border-right");
+  document.getElementById(`square-2-3`).classList.add("border-right");
+  document.getElementById(`square-3-3`).classList.add("border-right");
+  document.getElementById(`square-3-0`).classList.add("border-btm");
+  document.getElementById(`square-3-1`).classList.add("border-btm");
+  document.getElementById(`square-3-2`).classList.add("border-btm");
+  document.getElementById(`square-3-3`).classList.add("border-btm");
 });
 
 document.getElementById("five-by-five").addEventListener("click", function () {
@@ -51,4 +75,21 @@ document.getElementById("five-by-five").addEventListener("click", function () {
     document.getElementById(`square-${i}-3`).classList.remove("hidden");
     document.getElementById(`square-${i}-4`).classList.remove("hidden");
   }
+  // Remove right border when clicking to 5 X 5 from 3 X 3
+  document.getElementById(`square-0-2`).classList.remove("border-right");
+  document.getElementById(`square-1-2`).classList.remove("border-right");
+  document.getElementById(`square-2-2`).classList.remove("border-right");
+  // Remove bottom border when clicking to 5 X 5 from 3 X 3
+  document.getElementById(`square-2-0`).classList.remove("border-btm");
+  document.getElementById(`square-2-1`).classList.remove("border-btm");
+  document.getElementById(`square-2-2`).classList.remove("border-btm");
+  // Remove right and bottom borders when clickong from 4 X 4 to 5 X 5
+  document.getElementById(`square-0-3`).classList.remove("border-right");
+  document.getElementById(`square-1-3`).classList.remove("border-right");
+  document.getElementById(`square-2-3`).classList.remove("border-right");
+  document.getElementById(`square-3-3`).classList.remove("border-right");
+  document.getElementById(`square-3-0`).classList.remove("border-btm");
+  document.getElementById(`square-3-1`).classList.remove("border-btm");
+  document.getElementById(`square-3-2`).classList.remove("border-btm");
+  document.getElementById(`square-3-3`).classList.remove("border-btm");
 });
